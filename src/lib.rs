20eb8cfd769e035edaa49abcd4fa889ae2d0b5f1
simopek <>
2022-03-23 22:57:11 +0100

    Improve code

--- src/lib.rs
+++ src/lib.rs
@@ -7,20 +7,12 @@ pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
 }
 
 pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
-    iter.enumerate().filter(|item| item.0 % 2 == 0).map(|x| x.1)
-}
-
-fn abs_num(num: i16) -> i16 {
-    if num > 0 {
-        num
-    } else {
-        -num
-    }
+    iter.step_by(2)
 }
 
 pub struct Position(pub i16, pub i16);
 impl Position {
     pub fn manhattan(&self) -> i16 {
-        abs_num(self.0) + abs_num(self.1)
+        self.0.abs() + self.1.abs()
     }
 }
